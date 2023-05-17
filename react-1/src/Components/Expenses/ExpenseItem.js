ed5dec81f1bf31f6e630f87cdc463057506bbfd0
Falcon00007 <yofalcon98@gmail.com>
2023-05-17 20:09:39 +0530

    Added functional buttons to update title and add 100$ using useState

--- react-1/src/Components/Expenses/ExpenseItem.js
+++ react-1/src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle]=useState(props.title);
+  const [amount, setAmount]=useState(props.amount);
+
+const clickHandler=()=>{
+  setTitle("Upated");
+  console.log(title);
+}
+const updateAmount=()=>{
+  setAmount(amount+100);
+  console.log(amount);
+}
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button
-        onClick={(e) => {
-          e.target.parentElement.remove();
-        }}> Delete Expense</button>
+        onClick={clickHandler}>Change Title</button>
+        <button
+        onClick={updateAmount}>Add Amount</button>
     </Card>
   );
 };
