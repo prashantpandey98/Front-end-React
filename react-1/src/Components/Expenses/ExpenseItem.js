c7e8364ead54e314fc3cd1f40d296d08ef310b60
Falcon00007 <yofalcon98@gmail.com>
2023-05-21 09:17:26 +0530

    managing states and adding filter component

--- react-1/src/Components/Expenses/ExpenseItem.js
+++ react-1/src/Components/Expenses/ExpenseItem.js
@@ -1,33 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle]=useState(props.title);
-  const [amount, setAmount]=useState(props.amount);
-
-const clickHandler=()=>{
-  setTitle("Upated");
-  console.log(title);
-}
-const updateAmount=()=>{
-  setAmount(amount+100);
-  console.log(amount);
-}
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button
-        onClick={clickHandler}>Change Title</button>
-        <button
-        onClick={updateAmount}>Add Amount</button>
     </Card>
   );
 };
