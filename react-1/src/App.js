a926fc03ea0468c8e9b3b57a5e91d4d4f2c407b7
Falcon00007 <yofalcon98@gmail.com>
2023-05-23 17:54:36 +0530

    Rendering the list dynamically in the output

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Expenses from './Components/Expenses/Expenses';
 import NewExpense from './Components/NewExpense/NewExpense';
 
-const App = () => {
-  const expenses = [
+
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -26,9 +26,13 @@ const App = () => {
     },
   ];
 
+  const App = () => {
+   const [expenses, setExpenses ] = useState(DUMMY_EXPENSES)
+
 const addExpenseHandler= (expense) =>{
-console.log('In App.js');
-console.log(expense);
+setExpenses((prevExpenses)=>{
+  return [expense, ...prevExpenses];
+});
 }
 
   return (
