183354c26b4cc0fbbdccfa4cefe1b8175375db8b
Prashant Pandey <yofalcon98@gmail.com>
2023-07-21 11:36:21 +0530

    Cart Open and Close (Task-6)

--- Restaurant_App/src/components/Cart/Cart.js
+++ Restaurant_App/src/components/Cart/Cart.js
@@ -2,30 +2,25 @@ import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 
 
-const dishes=[{ id: 'c1', name: 'Biryani', amount: 2, price: 49.99 },{ id: 'c2', name: 'Halwa', amount: 3, price: 19.99 }]
-
-const totalPrice= dishes.map(dish=>
-    +(dish.price) + +dish.price
-    );
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {dishes.map((item) => (
+      {[{ id: 'c1', name: 'Biryani', amount: 2, price: 49.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{totalPrice}</span>
+        <span>49.99</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
